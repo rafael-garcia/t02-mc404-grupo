2bfd27763eb003e089299c33ae040e0ee25e7412
Rafael Matheus Garcia <rafamg93@gmail.com>
2015-06-18 13:34:27 -0300

    Adicao de estrutura basica seguindo fluxo do lab08. Adicao dos enderecos das pilhas

--- soul_desc.s
+++ soul_desc.s
@@ -1,10 +1,4 @@
 @Camada SOUL:
-@
-@- Configurar GPIO (Entradas e saidas):
-@  .set REG_DR,                0x53F84000     @Data Register
-@  .set REG_GDIR,              0x53F84004     @Direction Register
-@  .set REG_PSR,               0x53F84008     @Pad status register - apenas para leitura
-@
 @00 FLAG            Entrada
 @01 TRIGGER         Saida
 @02 SONAR_MUX[0]    Saida
@@ -47,6 +41,7 @@
 @  .set TZIC_PRIORITY9,        0x424
 @
 @- Configurar GPT (Com tempo razoável)
+@  .set TIME_SZ,            0x00000010 @ decimal = 16 
 @  .set GPT_CR,             0x53FA0000
 @  .set GPT_PR,             0x53FA0004
 @  .set GPT_SR,             0x53FA0008
@@ -55,3 +50,41 @@
 @  
 @Mudar para nivel usuario
 @Chamar codigo de controle
+
+.org 0x0
+.section .iv,"a"
+
+_start:     
+
+interrupt_vector:
+    b RESET_HANDLER
+.org 0x08
+    b SVC_HANDLER
+
+.org 0x100
+.text
+
+.set DATA_BASE_ADDR, 0x77801800 @ Parte da memoria destinada aos dados (definido no Makefile do projeto)
+
+@Configuracao de pilhas - cada uma tem 0x800 enderecos = 2KB
+.set USER_STACK,       DATA_BASE_ADDR
+.set FIQ_STACK,        0x77801000
+.set SUPERVISOR_STACK, 0x77800800
+.set ABORT_STACK,      0x77800000
+.set IRQ_STACK,        0x777FF800
+.set UNDEFINED_STACK,  0x777FF000
+
+@ Configurar GPIO (entradas e saidas)
+.set REG_DR,           0x53F84000     @Data Register
+.set REG_GDIR,         0x53F84004     @Direction Register (n-bit = 0 -> entrada, n-bit = 1 -> saida)
+.set REG_PSR,          0x53F84008     @Pad status register - apenas para leitura
+
+RESET_HANDLER:
+    @Set interrupt table base address on coprocessor 15.
+    ldr r0, =interrupt_vector
+    mcr p15, 0, r0, c12, c0, 0
+
+    b SET_GPT @ pula para o bloco de codigo que configura o GPT (e logo depois ja confiugra o TZIC)
+
+
+.data
