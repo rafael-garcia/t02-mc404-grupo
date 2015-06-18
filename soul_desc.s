a66b6e9d16be563c9721d0513a9cc23acd92a38f
Rafael M. Garcia <rafamg93@gmail.com>
2015-06-18 15:57:37 -0300

    Correcao dos sets

--- soul_desc.s
+++ soul_desc.s
@@ -61,7 +61,7 @@ interrupt_vector:
 .set REG_PSR,          0x53F84008     @Pad status register - apenas para leitura
 
 @Configuracao de mascaras para o GPIO
-.set MASK_GDIR         0b11111111111111000000000000111110
+.set MASK_GDIR,        0b00000000000000000000000000000000
 
 @ Configura enderecos TZIC
 .set TZIC_BASE,        0x0FFFC000
@@ -79,7 +79,7 @@ interrupt_vector:
 .set GPT_IR,           0x53FA000C
 
 @ Configura frequencia para fazer a contagem (system time)
-.set TIME_SZ           100
+.set TIME_SZ,          100
 
 @ Configura valor de iteracoes para aguardar algo entre 10-15 ms
 .set LOOP_WAITING_VAL  15000
