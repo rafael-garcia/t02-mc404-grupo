3abb412021993ab6a45e2932e8c7fdbe45f27d06
Rafael M. Garcia <rafamg93@gmail.com>
2015-06-18 14:24:30 -0300

    Adicionando mais valores globais (sets) Setando pilhas dos modos de execucao

--- soul_desc.s
+++ soul_desc.s
@@ -32,14 +32,6 @@
 @30 MOTOR1_SPEED[4] Saida
 @31 MOTOR1_SPEED[5] Saida
 @
-@- Configurar GPT (Com tempo razoável)
-@  .set TIME_SZ,            0x00000010 @ decimal = 16 
-@  .set GPT_CR,             0x53FA0000
-@  .set GPT_PR,             0x53FA0004
-@  .set GPT_SR,             0x53FA0008
-@  .set GPT_IR,             0x53FA000C
-@  .set GPT_OCR1,           0x53FA0010
-@  
 @Mudar para nivel usuario
 @Chamar codigo de controle
 
@@ -61,7 +53,7 @@ interrupt_vector:
 .set SUPERVISOR_STACK, 0x77800800
 .set ABORT_STACK,      0x77800000
 .set IRQ_STACK,        0x777FF800
-.set UNDEFINED_STACK,  0x777FF000
+.set LOCO_STACK,       0x777FF000
 
 @ Configura enderecos GPIO (entradas e saidas)
 .set REG_DR,           0x53F84000     @Data Register
@@ -80,30 +72,105 @@ interrupt_vector:
 .set TZIC_PRIORITY9,   0x424
 
 @ Configura enderecos GPT
-.set GPT_CR,                0x53FA0000
-.set GPT_PR,                0x53FA0004
-.set GPT_SR,                0x53FA0008
-.set GPT_OCR1,              0x53FA0010
-.set GPT_IR,                0x53FA000C
-
+.set GPT_CR,           0x53FA0000
+.set GPT_PR,           0x53FA0004
+.set GPT_SR,           0x53FA0008
+.set GPT_OCR1,         0x53FA0010
+.set GPT_IR,           0x53FA000C
 
 @ Configura frequencia para fazer a contagem (system time)
-.set TIME_SZ                100
+.set TIME_SZ           100
 
 @ Configura valor de iteracoes para aguardar algo entre 10-15 ms
-.set LOOP_WAITING_VAL       15000
+.set LOOP_WAITING_VAL  15000
 
 .org 0x100
 .text
+.align 4
+@ Zera o contador de tempo
+  ldr r2, =CONTADOR_TEMPO
+  mov r0, #0
+  str r0, [r2]
 
 RESET_HANDLER:
     @Set interrupt table base address on coprocessor 15.
     ldr r0, =interrupt_vector
     mcr p15, 0, r0, c12, c0, 0
 
-    b SET_GPT @ pula para o bloco de codigo que configura o GPT (e logo depois ja confiugra o TZIC)
+@ enderecos encontrados no datasheet IMX53-gpt.pdf na pagina do lab08 (tabela da pag 06)
+SET_GPT:     
+    ldr r3, =GPT_CR
+    mov r4, #0x00000041         @ clock_src periferico
+    str r4, [r3]
+
+    ldr r3, =GPT_PR
+    mov r4, #0                  @ zera o prescaler
+    str r4, [r3]
+
+    ldr r3, =GPT_OCR1
+    ldr r4, =TIME_SZ             @ conta ate a constante definida
+    str r4, [r3]
+
+    ldr r3, =GPT_IR
+    mov r4, #1                  @ habilita interrupcao do tipo Output Compare Channel 1
+    str r4, [r3]
+
+SET_TZIC:
+    @ Liga o controlador de interrupcoes
+    @ R1 <= TZIC_BASE
+
+    ldr r1, =TZIC_BASE
+
+    @ Configura interrupcao 39 do GPT como nao segura
+    mov r0, #(1 << 7)
+    str r0, [r1, #TZIC_INTSEC1]
+
+    @ Habilita interrupcao 39 (GPT)
+    @ reg1 bit 7 (gpt)
+
+    mov r0, #(1 << 7)
+    str r0, [r1, #TZIC_ENSET1]
+
+    @ Configure interrupt39 priority as 1
+    @ reg9, byte 3
+
+    ldr r0, [r1, #TZIC_PRIORITY9]
+    bic r0, r0, #0xFF000000
+    mov r2, #1
+    orr r0, r0, r2, lsl #24
+    str r0, [r1, #TZIC_PRIORITY9]
+
+    @ Configure PRIOMASK as 0
+    eor r0, r0, r0
+    str r0, [r1, #TZIC_PRIOMASK]
+
+    @ Habilita o controlador de interrupcoes
+    mov r0, #1
+    str r0, [r1, #TZIC_INTCTRL]
+
+SET_STACKS:
+    msr CPSR_c, #0x13       @ SUPERVISOR mode, IRQ/FIQ enabled
+    ldr sp, =SUPERVISOR_STACK
+
+    msr CPSR_c, #0x13       @ SUPERVISOR mode, IRQ/FIQ enabled
+    ldr sp, =SUPERVISOR_STACK
+
+
+    @instrucao msr - habilita interrupcoes
+    msr CPSR_c, #0xDF       @ SYSTEM mode, IRQ/FIQ disabled
+    ldr sp, =USER_STACK
+
+    msr CPSR_c, #0xD2       @ IRQ mode, IRQ/FIQ disabled
+    ldr sp, =IRQ_STACK
+
+    msr CPSR_c, #0x13       @ SUPERVISOR mode, IRQ/FIQ enabled
+    ldr sp, =SUPERVISOR_STACK
+
+    msr CPSR_c, #0x10       @ USER mode, IRQ/FIQ enabled
+    ldr sp, =LOCO
+
 
 SVC_HANDLER:
 
 .data
-CONTADOR_TEMPO: .word 0
+  CONTADOR_TEMPO: .word 0
