aa75b710815cdf9734faeb073e6507b8007c3182
Rafael M. Garcia <rafamg93@gmail.com>
2015-06-22 21:14:27 -0300

    o LOOP estava travando o codigo - Testando outras funcoes no loco Conflicts: 	soul.s

--- loco.c
+++ loco.c
@@ -1,7 +1,14 @@
 #include "api_robot2.h"
 
 void _start(void) {
-
-	set_motors_speed(30,30);
-
+	set_motor_speed(20,0);
+	delay();
+	set_motor_speed(20,1);
 }
+
+/* Spend some time doing nothing. */
+void delay() {
+  int i;
+  /* Not the best way to delay */
+  for(i = 0; i < 10000; i++ );
+}	
