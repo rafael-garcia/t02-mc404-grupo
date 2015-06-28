cb83c67eb505fe552510c990af72a597922de140
Rafael M. Garcia <rafamg93@gmail.com>
2015-06-28 18:41:36 -0300

    Testando o alarm de novo

--- loco.c
+++ loco.c
@@ -1,63 +1,39 @@
 #include "api_robot2.h"
 
 void delay();
-void anda_devagar();
-void gira_esq();
-void busca_parede();
-void segue_parede();
+void para();
+void anda();
+void gira_direita();
+void gira_esquerda();
 
+int i = 0;
 
 void _start(void) {
-	unsigned int tempo = get_time();
-	int i = 0;
-
-	set_motors_speed(15,15);
-	
-	for (; i < 50; i++) {
-		tempo++;
-		if ((tempo % 2) == 0) {
-			set_alarm(&anda_devagar, tempo);
-		} else {
-			set_alarm(&gira_esq, tempo);
-		}
-	}
+	unsigned int actual_time = 0;
+	unsigned int speed = 0;
+	actual_time = get_time();
+	set_alarm(&anda, actual_time + 2);
+	set_alarm(&para, actual_time + 4);
+	set_alarm(&gira_direita, actual_time + 6);
+	set_alarm(&para, actual_time + 8);
+	set_alarm(&anda, actual_time + 10);
+	while(1);
 }
 
-void anda_devagar() {
-	set_motors_speed(5,5);
+void para(){
+	set_motors_speed(0, 0);
 }
 
-void gira_esq() {
-	set_motors_speed(0,15);
+void anda(){
+	set_motors_speed(15, 15);
 }
 
-/**
-	Uoli anda em linha reta, sem colidir, ate encontrar uma parede a sua frente.
-*/
-void busca_parede() {
-	unsigned int girador = 0;
-	unsigned int sonar3 = 0;
-	unsigned int sonar4 = 0;
-	sonar3 = read_sonar(3);
-	sonar4 = read_sonar(4);
-
-	if (sonar3 > 1200 && sonar4 > 1200) {
-		set_motors_speed(30,30);
-	}
-	while (sonar3 > 1200 && sonar4 > 1200) {	
-		sonar3 = read_sonar(3);
-		sonar4 = read_sonar(4);
-	}
-	// ja esta proximo
-
-	while (girador < 25) {
-		set_motors_speed(0,10);
-		girador++;
-	}
+void gira_direita(){
+	set_motors_speed(10, 0);
 }
 
-void segue_parede() {
-	set_motors_speed(20,20);
+void gira_esquerda(){
+	set_motors_speed(0, 10);
 }
 
 /* Spend some time doing nothing. */
@@ -66,5 +42,4 @@ void delay() {
   /* Not the best way to delay */
   for(i = 0; i < 900; i++ ){
   }
-
-}		
+}
