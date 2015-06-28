40028fc74f5d0110d22879897dedccca56904861
Rafael M. Garcia <rafamg93@gmail.com>
2015-06-27 21:30:21 -0300

    Commitando o loco.c para testes do LSE (a logica dele ainda é indiferente quanto a resolução do problema proposto para o projeto)

--- loco.c
+++ loco.c
@@ -1,18 +1,43 @@
 #include "api_robot2.h"
 
 void delay();
-
-int i = 30;
+void busca_parede();
+void segue_parede();
 
 void _start(void) {
-	int a = 0;
-	set_motors_speed(30,30);
-	i = 0;
-	while(1){
-		if(read_sonar(4) < 2000){
-			set_motor_speed(10,1);
-		}
+	busca_parede();
+	// while (1) {
+		segue_parede();
+	// }
+}
+
+/**
+	Uoli anda em linha reta, sem colidir, ate encontrar uma parede a sua frente.
+*/
+void busca_parede() {
+	unsigned int girador = 0;
+	unsigned int sonar3 = 0;
+	unsigned int sonar4 = 0;
+	sonar3 = read_sonar(3);
+	sonar4 = read_sonar(4);
+
+	if (sonar3 > 1200 && sonar4 > 1200) {
+		set_motors_speed(30,30);
 	}
+	while (sonar3 > 1200 && sonar4 > 1200) {	
+		sonar3 = read_sonar(3);
+		sonar4 = read_sonar(4);
+	}
+	// ja esta proximo
+
+	while (girador < 25) {
+		set_motors_speed(0,10);
+		girador++;
+	}
+}
+
+void segue_parede() {
+	set_motors_speed(20,20);
 }
 
 /* Spend some time doing nothing. */
